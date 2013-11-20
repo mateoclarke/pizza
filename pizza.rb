c0ac2fe9acdb94bb3a8c5f6dc3fb1d17fa262378
mateoclarke <code@mateoclarke.com>
2013-11-20 15:24:11 -0600

    Add code that passes test for Pizza::Topping.new 'bell peppers', vegetarian: true.

--- pizza.rb
+++ pizza.rb
@@ -4,6 +4,16 @@ module Pizza
   end
 
   class Topping
-
+  	attr_accessor :name, :vegetarian 
+  	# Instantiate a new topping.
+  	#
+  	# name - The String name of the topping.
+  	# vegetarian - The Boolean indicating whether or not the topping is vegetarian.
+  	#
+  	# Returns a new Topping object.
+  	def initialize(name, vegetarian: false)
+  		@name = name
+  		@vegetarian = vegetarian
+  	end
   end
 end
